7ae3da2e46162f7ec713a9bbc101e366d6f295ad
Gothax <scott990328@gmail.com>
2025-01-12 14:41:21 +0900

    fix: 타임스탬프 찍히지 않는 버그 수정

--- be/src/main/java/com/yumst/be/global/entity/BaseTimeEntity.java
+++ be/src/main/java/com/yumst/be/global/entity/BaseTimeEntity.java
@@ -7,14 +7,17 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
     @CreatedDate
-    private Long createdAt;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
-    private Long updatedAt;
+    private LocalDateTime updatedAt;
+
 }
