0b79eb08c6df4431e257c0f9d352b2782c2f766d
Gothax <scott990328@gmail.com>
2025-01-12 19:26:18 +0900

    remove: 현재 버전에 맞지 않는 테스트 삭제

--- be/src/test/java/com/yumst/be/BeApplicationTests.java
+++ be/src/test/java/com/yumst/be/BeApplicationTests.java
@@ -1,13 +1,9 @@
 package com.yumst.be;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
