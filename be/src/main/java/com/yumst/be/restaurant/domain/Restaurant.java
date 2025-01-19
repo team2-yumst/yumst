5ca4a7b9764a10883d486af5961905966584d89a
Gothax <scott990328@gmail.com>
2025-01-19 17:09:31 +0900

    Revert "Add name field to Restaurant entity and update DTO"
    This reverts commit 8a53db196d9b1cf7b0e3d789113debc2fc05d342.

--- be/src/main/java/com/yumst/be/restaurant/domain/Restaurant.java
+++ be/src/main/java/com/yumst/be/restaurant/domain/Restaurant.java
@@ -24,9 +24,6 @@ public class Restaurant extends BaseTimeEntity {
     @Column(unique = true, nullable = false)
     private String restaurantId;
 
-    @Column(nullable = false)
-    private String name;
-
     @Embedded
     private Address address;
 
@@ -37,11 +34,10 @@ public class Restaurant extends BaseTimeEntity {
     private NaverInformation naverInformation;
 
     @Builder
-    public Restaurant(Address address, String name, OpenDataInformation openDataInformation, NaverInformation naverInformation) {
+    public Restaurant(Address address, OpenDataInformation openDataInformation, NaverInformation naverInformation) {
         this.restaurantId = UUID.randomUUID().toString();
 
         this.address = address;
-        this.name = name;
         this.openDataInformation = openDataInformation;
         this.naverInformation = naverInformation;
     }
