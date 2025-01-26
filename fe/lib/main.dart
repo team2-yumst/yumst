34341d0da70de5df02b1fab7750603695479c1e5
Gothax <scott990328@gmail.com>
2025-01-26 13:57:16 +0900

    feat: init project

---
+++ fe/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:fe/screens/register_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: RegisterPage(),
+      // debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+    );
+
+  }
+}
+
