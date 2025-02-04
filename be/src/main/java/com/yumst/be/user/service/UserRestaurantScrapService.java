6e33064a8f76666a5bfcdc40d537a9a48855db8d
Gothax <scott990328@gmail.com>
2025-02-04 23:59:03 +0900

    fix: restaurant id를 Long type으로 parse해서 생기는 버그 수정
    - restaurant repository에 findByRestaurantId 추가

--- be/src/main/java/com/yumst/be/user/service/UserRestaurantScrapService.java
+++ be/src/main/java/com/yumst/be/user/service/UserRestaurantScrapService.java
@@ -1,11 +1,10 @@
 package com.yumst.be.user.service;
 
-import com.yumst.be.restaurant.domain.Restaurant;
 import com.yumst.be.restaurant.repository.RestaurantRepository;
-import com.yumst.be.user.domain.UserEntity;
 import com.yumst.be.user.domain.UserRestaurantScrap;
 import com.yumst.be.user.repository.UserRepository;
 import com.yumst.be.user.repository.UserRestaurantScrapRepository;
+import com.yumst.be.user.vo.response.ResponseScrap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,14 +20,15 @@ public class UserRestaurantScrapService {
     private final RestaurantRepository restaurantRepository;
 
     @Transactional
-    public UserRestaurantScrap toggleScrap(String userId, String restaurantId) {
+    public ResponseScrap toggleScrap(String userId, String restaurantId) {
         // 기존 스크랩 여부 확인
         Optional<UserRestaurantScrap> existingScrap = scrapRepository.findByUserIdAndRestaurantId(userId, restaurantId);
+        checkPresentUserRestaurant(userId, restaurantId);
 
         if (existingScrap.isPresent()) {
             // 이미 스크랩된 경우 삭제
             scrapRepository.delete(existingScrap.get());
-            return existingScrap.get(); // 삭제된 스크랩 반환
+            return new ResponseScrap(userId, restaurantId, false);
         } else {
             // 스크랩되지 않은 경우 추가
             UserRestaurantScrap newScrap = UserRestaurantScrap.builder()
@@ -36,18 +36,17 @@ public class UserRestaurantScrapService {
                     .restaurantId(restaurantId)
                     .build();
             scrapRepository.save(newScrap);
-
-            // 유저 조회
-            UserEntity user = userRepository.findByUserId(userId)
-                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
-
-            // 식당 조회
-            Restaurant restaurant = restaurantRepository.findById(Long.parseLong(restaurantId))
-                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 식당입니다."));
-
-
-
-            return newScrap;
+            return new ResponseScrap(userId, restaurantId, true);
         }
+    }
+
+    private void checkPresentUserRestaurant(String userId, String restaurantId) {
+        // 유저 조회
+        userRepository.findByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        // 식당 조회
+        restaurantRepository.findByRestaurantId(restaurantId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 식당입니다."));
     }
 }
