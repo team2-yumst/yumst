54c4660336d287fab7fa107a4d808137d3f697ad
Gothax <scott990328@gmail.com>
2025-02-21 14:35:58 +0900

    fix(be): 스크랩 엔드포인트 요청 스펙 변경
    - 의미와 도메인을 명확하게 하기 위해 URI 변경
    - 프론트엔드에서 Dio를 통해 userId를 자동으로 전달하기 때문에 RequestHeader로 변경

--- be/src/main/java/com/yumst/be/user/controller/UserRestaurantScrapController.java
+++ be/src/main/java/com/yumst/be/user/controller/UserRestaurantScrapController.java
@@ -4,23 +4,20 @@ import com.yumst.be.user.service.UserRestaurantScrapService;
 import com.yumst.be.user.vo.response.ResponseScrap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
-@RequestMapping("/v1/scrap")
 @RequiredArgsConstructor
+@RequestMapping("api/user/v1")
 public class UserRestaurantScrapController {
 
     private final UserRestaurantScrapService scrapService;
 
-    @PostMapping("/{userId}/{restaurantId}")
+    @PostMapping("/scrap/{restaurantId}")
     public ResponseEntity<ResponseScrap> scrap(
-            @PathVariable String userId,
+            @RequestHeader String userId,
             @PathVariable String restaurantId) {
 
         ResponseScrap responseScrap = scrapService.toggleScrap(userId, restaurantId);
