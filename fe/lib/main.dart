378f3d8f07ee62889470397cb597d529c9c0dcfb
Gothax <scott990328@gmail.com>
2025-02-24 17:53:50 +0900

    feat(fe): splash screen 구현
    - 앱을 켰을때 작동
    - 로그아웃 했을때 작동
    - check auth 메서드로 상태코드를 받고 401시 로그인 페이지로 이동, 200이면 추천 페이지로 이동

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:fe/view/screen/main_page.dart';
-import 'package:fe/view/screen/recommendation_page.dart';
-import 'package:fe/view/screen/register_first_selection_page.dart';
 import 'package:fe/view/screen/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -14,23 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp(
-    //   home: FirstRegisterSelection(),
-    // );
-
-    // return MaterialApp(
-    //   home: SplashScreen(),
-    //   // debugShowCheckedModeBanner: false,
-    //   theme: ThemeData(
-    //     useMaterial3: true,
-    //     // colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-    //   ),
-    // );
-
-
     return MaterialApp(
-      home: const MainScreen(),
-      // debugShowCheckedModeBanner: false,
+      home: const SplashScreen()
     );
   }
 }
+
