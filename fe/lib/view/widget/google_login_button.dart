650efbb1b83f7007dd62c0bb9e2cdbccfb4de0d0
Gothax <scott990328@gmail.com>
2025-03-09 18:52:00 +0900

    fix: 회원가입 로직 진행 후 약관 동의 페이지로 넘어가도록 수정

--- fe/lib/view/widget/google_login_button.dart
+++ fe/lib/view/widget/google_login_button.dart
@@ -1,6 +1,6 @@
 
 import 'package:fe/repository/auth_repository.dart';
-import 'package:fe/view/screen/register_first_selection_page.dart';
+import 'package:fe/view/screen/auth_page/user_terms_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -29,7 +29,7 @@ class GoogleLoginButton extends ConsumerWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const FirstRegisterSelection(),
+                    builder: (context) => UserTermsPage(),
                   ),
                 );
               } else {
