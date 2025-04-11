b283a13ad063e803019fdf454b1f54ca6d12836d
Junhyung Kim <82752784+Gothax@users.noreply.github.com>
2025-04-11 16:21:04 +0900

    fix: 약관 동의 응답 버그 수정 (#81)
    - embedded entity 정보를 담도록 static factory method로 수정

--- be/src/main/java/com/yumst/be/user/dto/UserDto.java
+++ be/src/main/java/com/yumst/be/user/dto/UserDto.java
@@ -1,6 +1,8 @@
 package com.yumst.be.user.dto;
 
 import com.yumst.be.restaurant.vo.ResponseRestaurant;
+import com.yumst.be.user.domain.UserEntity;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
@@ -21,4 +23,36 @@ public class UserDto {
     private boolean isEnabled;
 
     private List<ResponseRestaurant> scrap;
+
+
+    public static UserDto from(UserEntity user) {
+        return UserDto.builder()
+                .userId(user.getUserId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .imageUrl(user.getImageUrl())
+                .finishedSurvey(user.getUserTerms().isFinishedSurvey())
+                .agreedPrivacyPolicy(user.getUserTerms().isAgreedPrivacyPolicy())
+                .agreedTermsOfService(user.getUserTerms().isAgreedTermsOfService())
+                .agreedLocationTerms(user.getUserTerms().isAgreedLocationTerms())
+                .isEnabled(user.isEnabled())
+                .build();
+    }
+
+    @Builder
+    public UserDto(String userId, String name, String email, String imageUrl, boolean finishedSurvey, boolean agreedPrivacyPolicy, boolean agreedTermsOfService, boolean agreedLocationTerms, boolean isEnabled, List<ResponseRestaurant> scrap) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.imageUrl = imageUrl;
+        this.finishedSurvey = finishedSurvey;
+        this.agreedPrivacyPolicy = agreedPrivacyPolicy;
+        this.agreedTermsOfService = agreedTermsOfService;
+        this.agreedLocationTerms = agreedLocationTerms;
+        this.isEnabled = isEnabled;
+        this.scrap = scrap;
+    }
+
+    public UserDto() {
+    }
 }
