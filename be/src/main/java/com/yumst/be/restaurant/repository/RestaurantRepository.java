f4c6ff3c0fcda0ad81945f463de5fc73da9303e1
Gothax <scott990328@gmail.com>
2025-04-13 01:45:47 +0900

    feat: 테스트를 위한 repository 코드 추가

--- be/src/main/java/com/yumst/be/restaurant/repository/RestaurantRepository.java
+++ be/src/main/java/com/yumst/be/restaurant/repository/RestaurantRepository.java
@@ -2,6 +2,7 @@ package com.yumst.be.restaurant.repository;
 
 import com.yumst.be.restaurant.domain.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +10,13 @@ import java.util.Optional;
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     Optional<Restaurant> findByRestaurantId(String restaurantId);
     List<Restaurant> findTop10RestaurantsByCrawlCompleteTrue();
+    @Query(
+                    "SELECT r " +
+                    "FROM Restaurant r " +
+                    "WHERE r.crawlComplete = true " +
+//                            "r.naverInformation.name like concat('%', '쭈꾸미블루스', '%') " +
+                    "ORDER BY r.naverInformation.rating DESC " +
+                    "LIMIT 10"
+    )
+    List<Restaurant> findTop10RestaurantsByCrawlCompleteTrueOrderByNaverInformation();
 }
