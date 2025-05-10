dadc91dd62dc7afaea6cec0632302fcb540ced11
experi-do <goodijdh@naver.com>
2025-05-11 06:13:20 +0900

    [feat] url 추가 -api/recommendation/v2/ai

--- recommendationServer/yumsRec/urls.py
+++ recommendationServer/yumsRec/urls.py
@@ -1,12 +1,10 @@
-from django.urls import include, path
-from rest_framework import routers
-
-from tutorial.quickstart import views
-
 from django.urls import path
-from recommendations.views import restaurant_recommendation_walk, restaurant_recommendation_vehicle
+from EquationRecommendations.views import restaurant_recommendation_walk, restaurant_recommendation_vehicle
+from AIRecommendations.views import restaurant_recommendation_ai
 
 urlpatterns = [
     path('api/recommendation/v1/walk', restaurant_recommendation_walk, name='walk_recommendation'),
     path('api/recommendation/v1/vehicle', restaurant_recommendation_vehicle, name='vehicle_recommendation'),
+path('api/recommendation/v2/ai', restaurant_recommendation_ai, name='ai_recommendation')
+
 ]
