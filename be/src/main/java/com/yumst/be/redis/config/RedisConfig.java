f7b2dd1e429e94bd635cd60128977f74deadd19c
Gothax <scott990328@gmail.com>
2025-05-14 19:54:46 +0900

    chore: stand alone 모드 설정으로 변경
    - aws elastic cache redis oss -> valkey로 변경 작업 포함

--- be/src/main/java/com/yumst/be/redis/config/RedisConfig.java
+++ be/src/main/java/com/yumst/be/redis/config/RedisConfig.java
@@ -2,19 +2,15 @@ package com.yumst.be.redis.config;
 
 import io.lettuce.core.ClientOptions;
 import io.lettuce.core.SocketOptions;
-import io.lettuce.core.cluster.ClusterClientOptions;
-import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import java.time.Duration;
-import java.util.List;
 
 @Configuration
 public class RedisConfig {
@@ -25,54 +21,34 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Value("${spring.data.redis.cluster.nodes}")
-    private List<String> nodes;
-
-    @Value("${spring.data.redis.cluster.max-redirects}")
-    private int maxRedirects;
-
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        final List<RedisNode> redisNodes = nodes.stream()
-                .map(node -> new RedisNode(node.split(":")[0], Integer.parseInt(node.split(":")[1])))
-                .toList();
+    public RedisConnectionFactory factory() {
 
-        // (1) Redis Cluster 설정
-        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
-        clusterConfiguration.setClusterNodes(redisNodes);
-        clusterConfiguration.setMaxRedirects(maxRedirects);
+        // standalone mode
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
 
-        // (2) Socket 옵션
+        // socket options
         SocketOptions socketOptions = SocketOptions.builder()
                 .connectTimeout(Duration.ofMillis(100L))
                 .keepAlive(true)
                 .build();
 
-        // (3) Cluster topology refresh 옵션
-        ClusterTopologyRefreshOptions clusterTopologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
-                .dynamicRefreshSources(true)
-                .enableAllAdaptiveRefreshTriggers()
-                .enablePeriodicRefresh(Duration.ofMinutes(30L))
-                .build();
-
-        // (4) Cluster Client 옵션
-        ClientOptions clientOptions = ClusterClientOptions.builder()
-                .topologyRefreshOptions(clusterTopologyRefreshOptions)
+        // client options
+        ClientOptions clientOptions = ClientOptions.builder()
                 .socketOptions(socketOptions)
                 .build();
 
-        // (5) Lettuce Client 옵션
+        // Lettuce Client option
         LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
                 .clientOptions(clientOptions)
                 .commandTimeout(Duration.ofMillis(3000L))
+                .useSsl()
                 .build();
 
-        return new LettuceConnectionFactory(clusterConfiguration, clientConfiguration);
+        return new LettuceConnectionFactory(configuration, clientConfiguration);
     }
-
-
-
-
 
 
 }
