aa48754c27811b610fd8175a9bfe11a283f30e3a
Gothax <scott990328@gmail.com>
2025-05-14 11:30:22 +0900

    chore: redis cluster 설정 추가

--- be/src/main/java/com/yumst/be/redis/config/RedisConfig.java
+++ be/src/main/java/com/yumst/be/redis/config/RedisConfig.java
@@ -1,10 +1,20 @@
 package com.yumst.be.redis.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.time.Duration;
+import java.util.List;
 
 @Configuration
 public class RedisConfig {
@@ -15,10 +25,52 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.cluster.nodes}")
+    private List<String> nodes;
+
+    @Value("${spring.data.redis.cluster.max-redirects}")
+    private int maxRedirects;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        final List<RedisNode> redisNodes = nodes.stream()
+                .map(node -> new RedisNode(node.split(":")[0], Integer.parseInt(node.split(":")[1])))
+                .toList();
+
+        // (1) Redis Cluster 설정
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        clusterConfiguration.setClusterNodes(redisNodes);
+        clusterConfiguration.setMaxRedirects(maxRedirects);
+
+        // (2) Socket 옵션
+        SocketOptions socketOptions = SocketOptions.builder()
+                .connectTimeout(Duration.ofMillis(100L))
+                .keepAlive(true)
+                .build();
+
+        // (3) Cluster topology refresh 옵션
+        ClusterTopologyRefreshOptions clusterTopologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+                .dynamicRefreshSources(true)
+                .enableAllAdaptiveRefreshTriggers()
+                .enablePeriodicRefresh(Duration.ofMinutes(30L))
+                .build();
+
+        // (4) Cluster Client 옵션
+        ClientOptions clientOptions = ClusterClientOptions.builder()
+                .topologyRefreshOptions(clusterTopologyRefreshOptions)
+                .socketOptions(socketOptions)
+                .build();
+
+        // (5) Lettuce Client 옵션
+        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+                .clientOptions(clientOptions)
+                .commandTimeout(Duration.ofMillis(3000L))
+                .build();
+
+        return new LettuceConnectionFactory(clusterConfiguration, clientConfiguration);
     }
+
+
 
 
 
