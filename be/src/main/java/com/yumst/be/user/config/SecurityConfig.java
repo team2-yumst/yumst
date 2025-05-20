2e0d8c00c84b5e8b1895f80d64f1e31bec7d3410
Gothax <scott990328@gmail.com>
2025-05-21 02:00:08 +0900

    chore: rebuild backend
    - 추천서버 url 변경

--- be/src/main/java/com/yumst/be/user/config/SecurityConfig.java
+++ be/src/main/java/com/yumst/be/user/config/SecurityConfig.java
@@ -59,7 +59,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-
     // TODO: production 환경에서 설정 변경
     public UrlBasedCorsConfigurationSource apiCorsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
